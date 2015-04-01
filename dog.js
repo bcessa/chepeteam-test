598460e201a97a8e58bf2db666fd2b28e70764f2
Ben Cessa <ben@pixative.com>
2015-04-01 14:26:37 -0600

    Documentation
    Add JSDoc content to the 'dog' module to provide documentation

--- dog.js
+++ dog.js
@@ -1,27 +1,81 @@
+/**
+ * @module
+ * @author Chepeteam
+ * @version 0.1.0
+ * @license MIT
+ * @description Sample module to provide general background and information;
+ *              it's not meant to do anything useful.
+ */
+
 // Enable strict syntax mode
 'use strict';
 
+/**
+ * Dummy dog representation
+ * @constructor
+ * @param {string} name - Name of the dog, optional
+ * @param {string} race - Race of the dog, optional
+ * @example var myPet = new Dog( 'bolt' );
+ */
 function Dog( name, race ) {
+  /** 
+   * @member {string} - Dog's name
+   * @default 'doggy'
+   */
   this.name = name || 'doggy';
+  
+  /** 
+   * @member {int} - Dog's age in years
+   * @default 1
+   */
   this.age = 1;
+  
+  /** 
+   * @member {int} - Dog's weight in kg
+   * @default 1
+   */
   this.weight = 1;
+  
+  /**
+   * @member {string} - Dog's race
+   * @default 'chihuahua'
+   */
   this.race = race || 'chihuaha';
 }
 
+/**
+ * Make the dog bark
+ * @returns {string}
+ */
 Dog.prototype.bark = function() {
   return 'guau, guau!';
 }
 
+/**
+ * Make the dog eat some food, this will increment it's weight in 10%
+ * @param {int} amountOfFood - The quantity of food to consume
+ */
 Dog.prototype.eat = function( amountOfFood ) {
   this.weight += amountOfFood * 0.10;
 }
 
+/**
+ * Accesor method for the dog's weight property
+ * @returns {float} - The actual dog weight
+ */
 Dog.prototype.getWeight = function() {
   return this.weight;
 }
 
+/**
+ * Generate a JSON representation of the dog
+ * @returns {string}
+ */
 Dog.prototype.info = function() {
   return JSON.stringify( this );
 }
 
+/**
+ * Dog constructor
+ */
 module.exports = Dog;
